6b9a8b47c2dc976d4e7c2a6ab44fc9e241582b77
salty1308 <5107321+salty1308@users.noreply.github.com>
2018-06-05 17:44:20 +0100

    Update changeLicense.ps1

--- 365/changeLicense.ps1
+++ 365/changeLicense.ps1
@@ -1,4 +1,6 @@
-﻿$Office365EnterpriseE3_ExcludingExchangeOnline = New-MsolLicenseOptions -AccountSkuId hsypol:ENTERPRISEPACK -DisabledPlans EXCHANGE_S_ENTERPRISE,INTUNE_O365,SWAY,PROJECTWORKMANAGEMENT,YAMMER_ENTERPRISE,RMS_S_ENTERPRISE,OFFICESUBSCRIPTION,SHAREPOINTWAC
+﻿$tenantID = "tenant"
+ $entPack = ($tenant+":ENTERPRISEPACK")
+ $Office365EnterpriseE3_ExcludingExchangeOnline = New-MsolLicenseOptions -AccountSkuId $entPack -DisabledPlans EXCHANGE_S_ENTERPRISE,INTUNE_O365,SWAY,PROJECTWORKMANAGEMENT,YAMMER_ENTERPRISE,RMS_S_ENTERPRISE,OFFICESUBSCRIPTION,SHAREPOINTWAC
 
 $users = Get-MsolUser -all | Where-Object {$_.isLicensed -eq "TRUE"}
 
@@ -7,10 +9,10 @@ $Users | ForEach-Object {
 Write-Progress -Activity "Assigning Licenses" -PercentComplete ($i / $users.count) -Status $i
 $_.UserPrincipalName
 try{
-Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -RemoveLicenses hsypol:ENTERPRISEPACK #-ErrorAction Stop
-Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -AddLicenses hsypol:ENTERPRISEPACK -LicenseOptions $Office365EnterpriseE3_ExcludingExchangeOnline -ErrorAction Stop
+Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -RemoveLicenses $entPack #-ErrorAction Stop
+Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -AddLicenses $entPack -LicenseOptions $Office365EnterpriseE3_ExcludingExchangeOnline -ErrorAction Stop
 }catch{
-Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -AddLicenses hsypol:ENTERPRISEPACK -LicenseOptions $Office365EnterpriseE3_ExcludingExchangeOnline
+Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -AddLicenses $entPack -LicenseOptions $Office365EnterpriseE3_ExcludingExchangeOnline
 }
 $i++
 }
